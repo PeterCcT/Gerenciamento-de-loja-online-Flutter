d34f321962d54d1e67492482eed2095b434934cd
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-05-18 16:52:09 -0300

    Correção de problema com stream e login finalizado

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -1,24 +1,84 @@
+import 'dart:async';
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gerenciadorlojavirtual/validators/login_validator.dart';
 import 'package:rxdart/rxdart.dart';
 
+enum LoginState { IDLE, LOADING, SUCCESS, FAIL }
+
 class LoginBloc extends BlocBase with LoginValidator {
-  final _emailController = BehaviorSubject<String>();
-  final _passController = BehaviorSubject<String>();
+  final _emailController = StreamController<String>.broadcast();
+  final _passController = StreamController<String>.broadcast();
+  final _stateController = BehaviorSubject<LoginState>();
 
   Stream<String> get outEmail =>
       _emailController.stream.transform(validateEmail);
-  Stream<String> get ouPassword =>
+  Stream<String> get outPassword =>
       _passController.stream.transform(validateSenha);
 
   Stream<bool> get outSubmitValid =>
-      Observable.combineLatest2(outEmail, ouPassword, (email, pass) => true);
+      Observable.combineLatest2(outEmail, outPassword, (email, pass) => true);
+
+  Stream<LoginState> get outState => _stateController.stream;
+
+  StreamSubscription _subscription;
 
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePass => _passController.sink.add;
+
+  LoginBloc() {
+    _subscription =
+        FirebaseAuth.instance.onAuthStateChanged.listen((user) async {
+      if (user != null) {
+        if (await verifyAdm(user)) {
+          _stateController.add(LoginState.SUCCESS);
+        } else {
+          FirebaseAuth.instance.signOut();
+          _stateController.add(LoginState.FAIL);
+        }
+      } else {
+        _stateController.add(LoginState.IDLE);
+      }
+    });
+  }
+
   @override
   void dispose() {
     _emailController.close();
     _passController.close();
+    _stateController.close();
+    _subscription.cancel();
+  }
+
+  void submit() {
+    final email = outEmail.toString();
+    final senha = outPassword.toString();
+    _stateController.add(LoginState.LOADING);
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: senha)
+        .catchError(
+      (_) {
+        _stateController.add(LoginState.FAIL);
+      },
+    );
+  }
+
+  Future<bool> verifyAdm(FirebaseUser user) async {
+    return await Firestore.instance
+        .collection('admin')
+        .document(user.uid)
+        .get()
+        .then(
+      (doc) {
+        if (doc.data != null) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    ).catchError((error) {
+      return false;
+    });
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gerenciadorlojavirtual/blocs/login_bloc.dart';
+import 'package:gerenciadorlojavirtual/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,70 +9,131 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _loginBloc = LoginBloc();
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      body: Center(
-        heightFactor: 1.5,
-        child: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 18),
-            child: Column(
-              children: <Widget>[
-                Icon(
-                  Icons.lightbulb_outline,
-                  color: Colors.purple,
-                  size: 150,
-                ),
-                Form(
-                  child: Column(
-                    children: <Widget>[
-                      FormFild(
-                        Icons.email,
-                        'Email',
-                        TextInputType.emailAddress,
-                        false,
-                        _loginBloc.outEmail,
-                        _loginBloc.changeEmail,
-                      ),
-                      SizedBox(height: 10),
-                      FormFild(
-                        Icons.lock_outline,
-                        'Senha',
-                        TextInputType.visiblePassword,
-                        true,
-                        _loginBloc.ouPassword,
-                        _loginBloc.changePass,
-                      ),
-                      SizedBox(height: 20),
-                      StreamBuilder(
-                        stream: _loginBloc.outSubmitValid,
-                        builder: (context, snapshot) {
-                          return RaisedButton(
-                            disabledColor: Colors.grey,
-                            padding: EdgeInsets.symmetric(horizontal: 100),
-                            child: Text(
-                              'Entrar',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 17),
-                            ),
-                            color: Colors.purple,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)),
-                            onPressed: snapshot.hasData ? () {} : null,
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
+  void initState() {
+    super.initState();
+    _loginBloc.outState.listen((state) async {
+      switch (state) {
+        case LoginState.SUCCESS:
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => HomeScreen()));
+          break;
+        case LoginState.FAIL:
+          return showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Erro'),
+              content: Text(
+                  'Você não tem as permissões necessárias para acessar essa aba'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Ok')),
               ],
             ),
-          ),
-        ),
-      ),
-    );
+          );
+          break;
+        case LoginState.LOADING:
+        case LoginState.IDLE:
+        default:
+          return Container();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<LoginState>(
+        stream: _loginBloc.outState,
+        initialData: LoginState.LOADING,
+        builder: (context, snapshot) {
+          switch (snapshot.data) {
+            case LoginState.LOADING:
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.white),
+                ),
+              );
+            case LoginState.FAIL:
+            case LoginState.IDLE:
+            case LoginState.SUCCESS:
+              return Scaffold(
+                backgroundColor: Colors.grey[900],
+                body: Center(
+                  heightFactor: 1.5,
+                  child: SingleChildScrollView(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 18),
+                      child: Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.lightbulb_outline,
+                            color: Colors.purple,
+                            size: 150,
+                          ),
+                          Form(
+                            child: Column(
+                              children: <Widget>[
+                                FormFild(
+                                  Icons.email,
+                                  'Email',
+                                  TextInputType.emailAddress,
+                                  false,
+                                  _loginBloc.outEmail,
+                                  _loginBloc.changeEmail,
+                                ),
+                                SizedBox(height: 10),
+                                FormFild(
+                                  Icons.lock_outline,
+                                  'Senha',
+                                  TextInputType.visiblePassword,
+                                  true,
+                                  _loginBloc.outPassword,
+                                  _loginBloc.changePass,
+                                ),
+                                SizedBox(height: 20),
+                                StreamBuilder(
+                                  stream: _loginBloc.outSubmitValid,
+                                  builder: (context, snapshot) {
+                                    return RaisedButton(
+                                      disabledColor: Colors.grey,
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 100),
+                                      child: Text(
+                                        'Entrar',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 17),
+                                      ),
+                                      color: Colors.purple,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      onPressed: snapshot.hasData
+                                          ? _loginBloc.submit
+                                          : null,
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+              break;
+            default:
+              return Container();
+          }
+        });
   }
 }
 
